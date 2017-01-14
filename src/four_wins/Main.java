2e1bf8e8edc8b67d26582e8e10deb7ac19147e9f
stefan <stefanschneider88@googlemail.com>
2017-01-14 10:41:09 +0100

    Initial commit

---
+++ src/four_wins/Main.java
@@ -0,0 +1,11 @@
+package four_wins;
+
+/**
+ * Created by stefan on 14.01.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("just a test");
+    }
+}
